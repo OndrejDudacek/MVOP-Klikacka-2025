27e21b5e5993e3b9b990d9805e7049934d5dc849
OndrejDudacek <ondrej.dudacek@gmail.com>
2025-04-30 22:19:40 +0200

    Enhance reset game functionality to require minimum money; update button text for clarity

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,18 +1,21 @@
 <template>
 	<h2>Settings</h2>
 	<section>
-		<Button @click="resetGame">Resetuj hru pro vylepšní</Button>
+		<Button @click="resetGame"
+			>Resetuj hru pro vylepšní <strong>1 000 000 💵</strong></Button
+		>
 	</section>
 </template>
 
 <script setup lang="ts">
 	import { useGameStore } from "@/stores/game";
-	import { useUpgradesStore } from "@/stores/upgrades";
 	import Button from "@/components/Button.vue";
 
 	const gameStore = useGameStore();
 
 	const resetGame = () => {
-		gameStore.resetGame();
+		if (gameStore.money >= 1000000) {
+			gameStore.resetGame();
+		}
 	};
 </script>
